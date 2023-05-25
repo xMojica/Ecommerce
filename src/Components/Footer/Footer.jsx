7fb69186157680e8c8d32f9c91987010d65c3869
santiago <samoji08@gmail.com>
2023-05-25 12:49:25 -0500

    Footer
    Se hizo el footer y los hipervinculos

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import "./Footer.css"
 
+
+
 function Footer() {
+
     return (
         <footer>
-
+            <div id='div-logos'>
+                <span onClick={() => { window.open('https://www.instagram.com', '_blank') }}><box-icon name='instagram' type='logo' size="70px" color="#fff"></box-icon></span>
+                <span onClick={() => { window.open('https://www.facebook.com', '_blank') }}><box-icon name='facebook' type='logo' size="70px" color="#fff"></box-icon></span>
+                <span onClick={() => { window.open('https://www.youtube.com', '_blank') }}><box-icon name='youtube' type='logo' size="70px" color="#fff"></box-icon></span>
+            </div>
+            <hr />
+            <p>Todos los derechos reservados &copy; 2023</p>
+            <div id='div-super'>
+                <img src="https://http2.mlstatic.com/frontend-assets/ml-web-navigation/ui-navigation/5.21.22/mercadolibre/sic.png" alt="superintentendcia" />
+                <img src="https://http2.mlstatic.com/frontend-assets/ml-web-navigation/ui-navigation/5.21.22/mercadolibre/pum.png" alt="nopare" />
+            </div>
         </footer>
     )
 }
