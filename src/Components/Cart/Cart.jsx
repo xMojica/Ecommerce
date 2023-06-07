e2cf55fce3edf8a5b121d5d45da9be1cbefac605
santiago <samoji08@gmail.com>
2023-06-06 21:30:02 -0500

    Carrito 9/10
    Carrito listo, faltan solo validaciones de los mismos productos y ya

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import "./Cart.css"
 import { Context } from '../../Context/main'
 import Header from '../Header/Header'
@@ -10,19 +10,30 @@ function Cart() {
     const navigate = useNavigate()
     const context = useContext(Context)
     let total = 0;
+    const [newCart] = useState(context.cart)
 
     function delet(e) {
-
         context.cart.splice(e.target.id, 1);
         navigate("/Cart")
     }
 
-    function minus() {
+    function minus(e) {
+        if (newCart[e.target.id].amount && newCart[e.target.id].quantity > 1) {
+            newCart[e.target.id].quantity--;
+            newCart[e.target.id].total = newCart[e.target.id].quantity * newCart[e.target.id].price;
+            context.setCart(newCart)
+            navigate("/Cart")
+        }
 
     }
 
-    function plus() {
-
+    function plus(e) {
+        if (newCart[e.target.id].amount > newCart[e.target.id].quantity) {
+            newCart[e.target.id].quantity++;
+            newCart[e.target.id].total = newCart[e.target.id].quantity * newCart[e.target.id].price;
+            context.setCart(newCart)
+            navigate("/Cart")
+        }
     }
 
     return (
@@ -44,34 +55,34 @@ function Cart() {
                             {context.cart.map((e, index) => {
                                 total = total + e.total
                                 return (
-                                    <tbody>
-                                        <tr key={index}>
+                                    <tbody key={index}>
+                                        <tr>
                                             <td id='td-img'><img id="product-img" src={e.img} alt="Product" /></td>
                                             <td>{e.name}</td>
                                             <td>{e.price}</td>
                                             <td>{e.quantity}</td>
                                             <td>{e.total}</td>
                                             <td><i id={index} className='bx bx-trash bx-md' onClick={delet}></i></td>
-                                            <td><i className='bx bx-minus bx-md' onClick={minus}></i></td>
-                                            <td><i className='bx bx-plus bx-md' onClick={plus}></i></td>
+                                            <td><i id={index} className='bx bx-minus bx-md' onClick={minus}></i></td>
+                                            <td><i id={index} className='bx bx-plus bx-md' onClick={plus}></i></td>
                                         </tr>
                                     </tbody>
                                 )
                             })}
-                            <tbody>
+                            <tbody key="totalRow">
                                 <tr>
                                     <td></td>
                                     <td></td>
                                     <td></td>
                                     <td>Total:</td>
-                                    <td id='total'>{total}</td>
+                                    <td id='total'>$ {total} USD</td>
                                 </tr>
                             </tbody>
                         </table>
                     </div>
                 </div>
+            </main>
 
-            </main >
             <Footer />
         </>
     )
